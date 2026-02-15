cab3dc698d4da9eec782cae75311c2bf85dd4a5a
ga5347-coder <ga5347@srmist.edu.in>
2026-02-15 23:14:26 +0530

    Uc2 - Render OOPS banner using ASCII art and concatenation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,37 @@
 /**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * OOPSBannerApp UC2 - Banner Display Application
+ *
+ * Displays the word "OOPS" in a 7-line ASCII banner
+ * using asterisks (*) and spaces.
+ *
+ * @author Gayathri
+ * @version 2
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
+        // Line 1
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+
+        // Line 2
+        System.out.println(" *     * " + " *     * " + " *     * " + " *      ");
+
+        // Line 3
+        System.out.println("*       *" + "*       *" + "*     *  " + "*      ");
+
+        // Line 4
+        System.out.println("*       *" + "*       *" + "******   " + " *****  ");
+
+        // Line 5
+        System.out.println("*       *" + "*       *" + "*        " + "      * ");
+
+        // Line 6
+        System.out.println(" *     * " + " *     * " + " *       " + " *    *  ");
+
+        // Line 7
+        System.out.println("  *****  " + "  *****  " + " *        " + "  ****   ");
+
+    }
 }
