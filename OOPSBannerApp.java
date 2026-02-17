f59b4bd24ceb0233893a52cb64355171ed427f36
ga5347-coder <ga5347@srmist.edu.in>
2026-02-18 00:59:18 +0530

    UC1 - Print OOPS to Console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,37 +1,18 @@
 /**
- * OOPSBannerApp UC2 - Banner Display Application
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
  *
- * Displays the word "OOPS" in a 7-line ASCII banner
- * using asterisks (*) and spaces.
+ * This class demonstrates a simple Java application
+ * that displays the Object Oriented Programming System
+ * acronym OOPS to the console.
  *
- * @author Gayathri
- * @version 2
+ * @author Developer
+ * @version 1.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        // Line 1
-        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
-
-        // Line 2
-        System.out.println(" *     * " + " *     * " + " *     * " + " *      ");
-
-        // Line 3
-        System.out.println("*       *" + "*       *" + "*     *  " + "*      ");
-
-        // Line 4
-        System.out.println("*       *" + "*       *" + "******   " + " *****  ");
-
-        // Line 5
-        System.out.println("*       *" + "*       *" + "*        " + "      * ");
-
-        // Line 6
-        System.out.println(" *     * " + " *     * " + " *       " + " *    *  ");
-
-        // Line 7
-        System.out.println("  *****  " + "  *****  " + " *        " + "  ****   ");
-
+        System.out.println("OOPS");
     }
+
 }
